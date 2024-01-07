081d663884574300c143e35b45d63d606c6a0f9f
SennaMa <senna.ma@live.com>
2024-01-06 23:03:22 -0500

    wrapped in functions. organized utils. updated main

--- project/poke_api/main.py
+++ project/poke_api/main.py
@@ -1,45 +1,13 @@
-"""
-URL: https://pokeapi.co/api/v2/pokemon/35
+from utils import clean_descriptions, index_match, save_clean_df_to_csv
+import pandas as pd
+import os
 
-What's available:
-- sprites for visual component
-- species > url > generation > flavor_text (https://pokeapi.co/api/v2/pokemon-species/35/)
+cwd = os.getcwd()
+path_to_raw_df = f'{cwd}/data/poke_df.csv'
+path_to_clean_df = f'{cwd}/data/clean_poke_df.csv'
 
-Species Example: https://pokeapi.co/api/v2/pokemon-species/35/
-"""
+raw_df = pd.read_csv(path_to_raw_df)
+clean_df = clean_descriptions(raw_df)
+index_results = index_match(raw_df, clean_df)
 
-import requests
-import random
-
-def generate_poke_id() -> int:
-    """Generate a poke_id, starting from 001: Bulbasaur
-    to max poke_id 1017: Ogerpon.
-
-    Returns:
-        int: pokemon_id
-    """
-    return random.randrange(1,1018)
-
-def get_poke_description(poke_id: int) -> dict:
-    """Fetches data from PokeAPI.
-
-    Args:
-        poke_id (int): random integer, max 1017
-
-    Returns:
-        dict: json dictionary with pokemon data
-    """
-    r = requests.get(f'https://pokeapi.co/api/v2/pokemon-species/{poke_id}')
-    data = r.json()
-
-    # print(poke_id)
-    # print(data['name'])
-    # print(data['base_happiness'])
-
-    return data
-
-poke_id = generate_poke_id()
-poke_data = get_poke_description(poke_id)
-
-print(poke_data['name'])
-print(poke_data['base_happiness'])
+save_clean_df_to_csv(raw_df,clean_df,index_results,path_to_clean_df)
--- project/poke_api/utils.py
+++ project/poke_api/utils.py
@@ -2,55 +2,67 @@ import pandas as pd
 import os
 import re
 
-cwd = os.getcwd()
-path_to_raw_df = f'{cwd}/data/poke_df.csv'
-path_to_clean_df = f'{cwd}/data/clean_poke_df.csv'
 
-raw_df = pd.read_csv(path_to_raw_df)
+def clean_descriptions(raw_df:pd.DataFrame) -> pd.DataFrame:
+    """Clean raw text by (1) removing new lines markers,
+    esc sequences, and zero bytes, and (2) replacing the term
+    "POKéMON" with the pokemon's name for better haikus.
 
-# create a dictionary that stores the index and cleaned text
-clean_text = {'id': [], 'c_description':[]}
-c_description = []
-id_list = []
+    Args:
+        raw_df (pd.DataFrame): raw df with poke-descriptions.
 
-# iterate thorugh the df
-subset_df = raw_df[['id','name','description']]
+    Returns:
+        pd.DataFrame: clean dataframe.
+    """
+    # initialize dict that stores the index and clean text
+    clean_text = {'id': [], 'c_description':[]}
+    c_description = []
+    id_list = []
 
-subset_df = subset_df.iloc[:10]                     # extract top 10 rows
+    subset_df = raw_df[['id','name','description']]
+    # subset_df = subset_df.iloc[:10]                                   # test: extract top 10 rows
 
+    for index, row in subset_df.iterrows():
+        raw_text = row['description']
+        name = row['name'].upper()
+        
+        remove_new_line = re.sub('\n',' ', raw_text)
+        remove_esc_seq = re.sub('\u000c', ' ', remove_new_line)
+        remove_zero_bytes = re.sub('\x0c', ' ', remove_esc_seq)
 
-for index, row in subset_df.iterrows():
-    # replace "POKéMON" with the name of the pokemon for better results.
-    raw_text = row['description']
-    name = row['name'].upper()
-    
-    remove_new_line = re.sub('\n',' ', raw_text)
-    remove_esc_seq = re.sub('\u000c', ' ', remove_new_line)
-    remove_zero_bytes = re.sub('\x0c', ' ', remove_esc_seq)
+        substituted_text = remove_zero_bytes.replace("POKéMON",name)
+        
+        id_list.append(index)
+        c_description.append(substituted_text)
 
-    substituted_text = remove_zero_bytes.replace("POKéMON",name)
-    
-    id_list.append(index)
-    c_description.append(substituted_text)
+    clean_text['id'] = id_list
+    clean_text['c_description'] = c_description
 
-clean_text['id'] = id_list
-clean_text['c_description'] = c_description
-
-clean_df = pd.DataFrame(data=clean_text)
-
-# run check to ensure n rows are the same between df and dict.keys()
-
-
-
-
-# append new column to df
-final_df = pd.merge(raw_df, clean_df['c_description'], left_index=True, right_index=True)
-final_df.to_csv(path_to_clean_df, index=False)
+    clean_df = pd.DataFrame(data=clean_text)
+    return clean_df
 
 
-# next steps: wrap in functions
+def index_match(raw_df:pd.DataFrame, clean_df:pd.DataFrame) -> bool:
+    """Check that the number of rows are the same between both dataframes.
+    The only change should be the text in poke_description.
+
+    Args:
+        raw_df (pd.DataFrame): original df.
+        clean_df (pd.DataFrame): df with clean poke descriptions.
+
+    Returns:
+        bool: index match is true or false
+    """
+    if raw_df.index.stop != clean_df.index.stop:
+        return False
+    return True
 
 
-
-
-
+def save_clean_df_to_csv(raw_df: pd.DataFrame, clean_df: pd.DataFrame, index_check: bool, save_path: str):
+    if not index_check:
+        raise Exception("Index doesn't match when comparing the raw and clean df. Please review.")
+    else:
+        # append new column to df
+        final_df = pd.merge(raw_df, clean_df['c_description'], left_index=True, right_index=True)
+        final_df.to_csv(save_path, index=False)
+        print(f'CSV saved to {save_path}.')
