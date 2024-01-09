9fc69af13b886ee1de97e5193fc974767e4999d5
SennaMa <senna.ma@live.com>
2024-01-09 17:17:55 -0500

    adding logs

--- project/haiku_generator/main.py
+++ project/haiku_generator/main.py
@@ -2,19 +2,33 @@ from dotenv import load_dotenv
 from random import randint
 from utils import generate_haiku
 import pandas as pd
+import logging
 import os
 
+
+logging.basicConfig(filename="parameters.log", 
+					format='%(asctime)s %(message)s')
+
+
+logger=logging.getLogger()
+logger.setLevel(logging.DEBUG)
 load_dotenv()
 PERSONAL_TOKEN = os.getenv('COHERE_PERSONAL_TOKEN')
 MODEL= os.getenv('CUSTOM_MODEL')
 
+
 # TODO: fix paths
 cwd = os.getcwd()
 path_to_clean_df = f'{cwd}/project/poke_api/data/clean_poke_df.csv'
-
-# input
 df = pd.read_csv(path_to_clean_df)
 poke_id = randint(0,149)
 
-s = generate_haiku(PERSONAL_TOKEN, MODEL, df, poke_id)
-print(s)
+# setting k to 20 gives us less random, fun responses. increasing to 60 is more fun.
+# setting temp to 90 provides for more fun responses. 80 seems to be the sweet spot.
+TOKENS=25
+TEMPERATURE=0.8
+TOP_K=20
+
+logger.info(f'Generated haiku was creating using these parameters: tokens={TOKENS}, temperature={TEMPERATURE}, k={TOP_K}')
+haiku = generate_haiku(PERSONAL_TOKEN, MODEL, df, poke_id, TOKENS, TEMPERATURE, TOP_K)
+print(haiku)
--- project/haiku_generator/utils.py
+++ project/haiku_generator/utils.py
@@ -9,18 +9,17 @@ import os
 cwd = os.getcwd()
 path_to_clean_df = f'{cwd}/project/poke_api/data/clean_poke_df.csv'
 
-def generate_haiku(token:str, custom_model:str, df:pd.DataFrame, poke_id:int) -> str:
+def generate_haiku(token:str, custom_model:str, df:pd.DataFrame, poke_id:int, tokens, temperature, top_k) -> str:
     prompt_move = df.iloc[poke_id]['move']
     prompt_description = df.iloc[poke_id]['c_description']
     prompt_pokemon = df.iloc[poke_id]['name']
     prompt_habitat = df.iloc[poke_id]['habitat']
 
-    # setting k to 20 gives us less random, fun responses. increasing to 60 is more fun.
-    # setting temp to 90 provides for more fun responses. 80 seems to be the sweet spot.
+    
     llm_prompt = f'Generate a three-line haiku that about the Pokemon called "{prompt_pokemon}". Here is a description of {prompt_pokemon}: {prompt_description}. {prompt_pokemon} knows the move {prompt_move} and {prompt_pokemon} lives in {prompt_habitat}.'
-    set_max_tokens=25
-    set_temperature=0.80
-    set_k=20
+    set_max_tokens=tokens
+    set_temperature=temperature
+    set_k=top_k
 
     print(f'Haiku Generated for {prompt_pokemon}, poke-id: {poke_id}. It knows the move: {prompt_move}')
     
